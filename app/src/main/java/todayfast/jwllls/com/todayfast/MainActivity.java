2b6840dd14a27fb43fc68856d3289768501fe337
Android_林健文 <jianwen@fallchat.com>
2017-09-13 17:49:52 +0800

    今日快讯

---
+++ app/src/main/java/todayfast/jwllls/com/todayfast/MainActivity.java
@@ -0,0 +1,13 @@
+package todayfast.jwllls.com.todayfast;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
